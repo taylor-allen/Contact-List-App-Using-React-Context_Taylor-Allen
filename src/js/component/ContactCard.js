b35bca68923bf28d75e0ac5ef1c9e238bfcf9839
Taylor Allen <50384820+taylor-allen@users.noreply.github.com>
2024-11-06 21:17:28 +0000

    testing

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -3,39 +3,37 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const ContactCard = ({ contact }) => {
-  const { full_name, email, address, phone } = contact;
+  const { name, email, address, phone } = contact;
   const { actions } = useContext(Context);
 
   return (
     <div className="contact-card col-xl-4 col-sm-6 mb-3">
       <div className="bg-white rounded py-4 px-4 contact-card-border">
-        <div className="text-center">
-          <div className="image-circle">
-            <div className="profile-icon-container">
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
-                alt="profile-icon"
-                width="100"
-                className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
-              />
-            </div>
+        <div className="image-circle">
+          <div className="profile-icon-container">
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
+              alt="profile-icon"
+              width="100"
+              className="img-fluid rounded-circle img-thumbnail shadow-sm"
+            />
           </div>
-          <h5 className="mb-0 ">{full_name}</h5>
         </div>
 
-        <div className="row mt-2  icons-margin-left ">
+        <h5>{name}</h5>
+        <div className="row mt-2 icons-margin-left ">
           <div className="col-12">
             <div className="d-flex mb-2">
-              <i className="fas fa-at mr-2 icons"></i>
-              <p className="mb-0 text-margin-left">{email}</p>
+              <i className="fas fa-at icons"></i>
+              <p className="mb-0 px-3 text-start">{email}</p>
             </div>
             <div className="d-flex mb-2">
-              <i className="fas fa-map-marker-alt mr-2 icons"></i>
-              <p className="mb-0 text-margin-left ">{address}</p>
+              <i className="fas fa-map-marker-alt icons"></i>
+              <p className="mb-0 px-3 text-start ">{address}</p>
             </div>
             <div className="d-flex mb-2">
-              <i className="fas fa-mobile-alt mr-2 icons"></i>
-              <p className="mb-0 text-margin-left ">{phone}</p>
+              <i className="fas fa-mobile-alt icons"></i>
+              <p className="mb-0 px-3 text-start">{phone}</p>
             </div>
           </div>
         </div>
